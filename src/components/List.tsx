6160665582c07a59ac0fd07ebd118d9d11c79802
EstebanKiito <esteban.ortega@uc.cl>
2025-07-04 16:12:11 -0400

    Manejo de Estados - hook useState

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,8 @@
 // Shift + Comand + R: Snippet with react TSRFCE:
 // Create a React Functional Component
 
+import { useState } from "react";
+
 // import { MouseEvent } from "react";
 
 type Props = {
@@ -8,19 +10,24 @@ type Props = {
 };
 
 function List({ data }: Props) {
+  // Componente List: Ahora con State: Se volvera a renderizar cuando se cambie el estado!
+  // Todos los componentes tienen estados diferentes!
+  const [index, setIndex] = useState(1); // HOOK: UseState: Retorna [var, function actualizadora de estado]
+
   // const handleClick = (e: MouseEvent) => { Asi recibo el evento
-  const handleClick = (element: string) => {
-    console.log(element);
+  const handleClick = (i: number) => {
+    //index = i; // Sin Estados: React no sabe que ha cambiado el valor de index
+    setIndex(i); // Con Estados: React sabe que ha cambiado el valor de index
   };
   //TIP:  Comand + D, tomar todas las var con mismo nombre!
   return (
     <ul className="list-group">
-      {data.map((elemento) => (
+      {data.map((elemento, i) => (
         //<li onClick={handleClick} key={elemento} className="list-group-item">
         <li
-          onClick={() => handleClick(elemento)}
+          onClick={() => handleClick(i)}
           key={elemento}
-          className="list-group-item"
+          className={`list-group-item ${index == i ? "active" : ""}`}
         >
           {elemento}
         </li>
