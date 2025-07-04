9771d212ff7ab517e627311e72e81e5e35f40ec6
EstebanKiito <esteban.ortega@uc.cl>
2025-07-04 15:32:03 -0400

    Manejo de Eventos

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,16 +1,27 @@
 // Shift + Comand + R: Snippet with react TSRFCE:
 // Create a React Functional Component
 
+// import { MouseEvent } from "react";
+
 type Props = {
   data: string[];
 };
 
 function List({ data }: Props) {
+  // const handleClick = (e: MouseEvent) => { Asi recibo el evento
+  const handleClick = (element: string) => {
+    console.log(element);
+  };
   //TIP:  Comand + D, tomar todas las var con mismo nombre!
   return (
     <ul className="list-group">
       {data.map((elemento) => (
-        <li key={elemento} className="list-group-item">
+        //<li onClick={handleClick} key={elemento} className="list-group-item">
+        <li
+          onClick={() => handleClick(elemento)}
+          key={elemento}
+          className="list-group-item"
+        >
           {elemento}
         </li>
       ))}
