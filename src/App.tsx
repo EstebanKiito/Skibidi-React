b62cf081c06ba75acf3d62b35157af219b498099
EstebanKiito <esteban.ortega@uc.cl>
2025-07-05 11:24:13 -0400

    Funciones como props: manejo de selección en el componente List

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import List from "./components/List";
 function App() {
   // Con ctrl+esp podemos ver las sugerencias de codigo
   const list = ["Goku", "Vegeta", "Gohan"];
+
+  const handleSelect = (elemento: string) => {
+    console.log("Elemento seleccionado:", elemento);
+  };
   return (
     <Card>
       <CardBody title="Hola Mundo" text="texto del componente" />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
     </Card>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -7,25 +7,28 @@ import { useState } from "react";
 
 type Props = {
   data: string[];
+  onSelect?: (elemento: string) => void; // onSelect es una funcion que recibe un string
 };
 
-function List({ data }: Props) {
+function List({ data, onSelect }: Props) {
   // Componente List: Ahora con State: Se volvera a renderizar cuando se cambie el estado!
   // Todos los componentes tienen estados diferentes!
   const [index, setIndex] = useState(1); // HOOK: UseState: Retorna [var, function actualizadora de estado]
 
   // const handleClick = (e: MouseEvent) => { Asi recibo el evento
-  const handleClick = (i: number) => {
+  const handleClick = (i: number, elemento: string) => {
     //index = i; // Sin Estados: React no sabe que ha cambiado el valor de index
     setIndex(i); // Con Estados: React sabe que ha cambiado el valor de index
+    onSelect?.(elemento); // ?. : Si onSelect esta definida, la llamo
   };
+
   //TIP:  Comand + D, tomar todas las var con mismo nombre!
   return (
     <ul className="list-group">
       {data.map((elemento, i) => (
         //<li onClick={handleClick} key={elemento} className="list-group-item">
         <li
-          onClick={() => handleClick(i)}
+          onClick={() => handleClick(i, elemento)}
           key={elemento}
           className={`list-group-item ${index == i ? "active" : ""}`}
         >
