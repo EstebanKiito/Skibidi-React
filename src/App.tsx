6d82c7a290b9ad13031422ffd09f15943ea59781
EstebanKiito <esteban.ortega@uc.cl>
2025-07-09 12:55:19 -0400

    Limpieza de App para ver gestion de estados

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,7 @@
-// import Card from "./components/Card";
-import Card, { CardBody } from "./components/Card";
-import List from "./components/List";
-import Button from "./components/Button"; // Si buscamos dentro de button, index es el archivo que importara por defecto
 import { useState } from "react";
 
 function App() {
-  // BUTTON HANDLING: Parte en falso el estado de isLoading
-  //const [isLoading, setIsLoading] = useState(false); // Hook de estado: Retorna un array con el estado y la funcion para actualizarlo
-  //const handleClick = () => setIsLoading(!isLoading); // Cambia el estado de loading al contrario del actual
-
-  const [data, setData] = useState(["Goku", "Vegeta", "Gohan"]); // Estado para la lista de datos
-
-  const addMiniom = () => {
-    console.log("Añadiendo Miniom");
-    setData([...data, "Miniom"]); // Agrega un nuevo elemento a la lista
-  };
-
-  const removeMiniom = () => {
-    console.log("Eliminando Miniom");
-    setData([...data.slice(0, -1)]); // Elimina el último elemento de la lista
-  };
-
-  // Con ctrl+esp podemos ver las sugerencias de codigo
-  //const list = ["Goku", "Vegeta", "Gohan"];
-
-  //const handleSelect = (elemento: string) => {
-  //  console.log("Elemento seleccionado:", elemento);
-  //};
-
-  //  CONDICIONALES PEQUEÑOS: dentro del codigo
-
-  // {list.length !== 0 && <List data={list} onSelect={handleSelect} />}
-  return (
-    <Card>
-      <CardBody />
-      <Button onClick={addMiniom}>Agregar</Button>
-      <Button onClick={removeMiniom}>Eliminar</Button>
-      <List data={data} />
-    </Card>
-  );
+  return <button>Boton</button>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import App from "./App.tsx";
-import AlertApp from "./ComponentsApp.tsx";
+import App from "./App.tsx";
+//import AlertApp from "./ComponentsApp.tsx";
+//import ButtonApp from "./ComponentsApp.tsx";
 // import "./index.css";
 //import "bootstrap/dist/css/bootstrap.min.css"; // Importamos Bootstrap
 
 // ReactDOM es el objeto que nos permite renderizar nuestro componente en el DOM (Podria ser ReactNative para apps moviles)
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AlertApp />
+    <App />
   </React.StrictMode>
 );
