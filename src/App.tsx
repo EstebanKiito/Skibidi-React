67f2e9c2121d0e8d1b1a3bf1cffacc8e89a8b031
EstebanKiito <esteban.ortega@uc.cl>
2025-07-09 13:54:09 -0400

    Estados y variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { useState } from "react";
 
+/** ---- PASOS ----
+ * 1. Envia la instruccion a React
+ * 2. Al terminar handleCount: Renderiza el componente
+ * 3. Actualiza el estado
+ */
+
 function App() {
-  return <button>Boton</button>;
+  let numero = 0; // Esto siempre se reinicializa a 0 cada vez que se renderiza el componente
+  // Mejor usar useState (por si queremos tener ese valor en cada componente)
+
+  console.log("Renderizando..."); // 2 veces en caso de que se use StrictMode en main.tsx
+  const [count, setCount] = useState(0);
+  const handleCount = () => {
+    numero++;
+    setCount(count + 1);
+    console.log("Count: ", count, numero); // No se actualizara aun / numero siempre sera 1
+  }; // Luego de terminar la funcion, se seteara el valor
+
+  return <button onClick={handleCount}>Enviar</button>;
 }
 
 export default App;
