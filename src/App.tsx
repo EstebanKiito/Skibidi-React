f452b66bd4ac3ede99fc27bf8024939c487bcefe
EstebanKiito <esteban.ortega@uc.cl>
2025-07-10 15:10:01 -0400

    Combinacion de estados

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,20 @@ function App() {
   let numero = 0; // Esto siempre se reinicializa a 0 cada vez que se renderiza el componente
   // Mejor usar useState (por si queremos tener ese valor en cada componente)
 
+  // --- Combinar Estados ---
+  const [user, setUser] = useState({
+    name: "Esteban",
+    lastName: "Pro",
+    age: 20,
+  });
+
+  const [shape, setShape] = useState({
+    width: 100,
+    height: 100,
+    x: 25,
+    y: 150,
+  });
+
   console.log("Renderizando..."); // 2 veces en caso de que se use StrictMode en main.tsx
   const [count, setCount] = useState(0);
   const handleCount = () => {
