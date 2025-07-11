698301d2c4465f096d0c3ca525da4bc7eb56c86e
EstebanKiito <esteban.ortega@uc.cl>
2025-07-10 20:54:17 -0400

    Actualizando Objetos

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,22 @@ import { useState } from "react";
  */
 
 function App() {
-  const [prodcuts, setProducts] = useState([{ name: "Iphone" }]);
+  const [prodcut, setProduct] = useState({ name: "Iphone", stock: 10 });
   const handleClick = () => {
-    //prodcuts.push({ name: "Samsung" }); // Metodo Mutable: Toma la variable de products y la modifica directamente
-    // El hook detecta cuando le pasamos una referencia nueva
-    // No si modificamos el objeto directamente
-    //1. let newProducts = prodcuts.concat({ name: "Xiaomi" }); // Metodo Inmutable: Crea una nueva referencia
-    //2. let newProducts = [...prodcuts, { name: "Xiaomi" }]; // Metodo Inmutable: Crea una nueva referencia
-    setProducts((prevState) => [...prevState, { name: "Xiaomi" }]); // Metodo Inmutable: Crea una nueva referencia
+    // --- ACTUALIZANDO OBJETOS ---
+
+    //prodcut.name = "Samsung"; // Mutacion: Modifica el objeto original
+    const newProduct = { ...prodcut, name: "Samsung" }; // Copia el objeto y modifica el nombre
+
+    setProduct(newProduct); // Metodo Inmutable: Crea una nueva referencia
   };
 
   return (
     <div>
-      <button onClick={handleClick}>Enviar</button>;
-      <ul>
-        {prodcuts.map((prodcuts) => (
-          <li key={prodcuts.name}>{prodcuts.name}</li>
-        ))}
-      </ul>
+      <button onClick={handleClick}>Agregar</button>
+      <p>
+        {prodcut.name} , {prodcut.stock}
+      </p>
     </div>
   );
 }
