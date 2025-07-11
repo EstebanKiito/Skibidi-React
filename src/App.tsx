a91c7d90bc2a5b2a6ac6bb18308db7ec954fe862
EstebanKiito <esteban.ortega@uc.cl>
2025-07-10 23:02:04 -0400

    EJERCICIO: Componentes Botones y Estados

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
 import { useState } from "react";
-import ProductDashboard from "./components/ProductDashboard.tsx";
 import ProductList from "./components/ProductList.tsx";
 import Button from "./components/Button.tsx";
-
-/** ---- PASOS ----
- * 1. Envia la instruccion a React
- * 2. Al terminar handleCount: Renderiza el componente
- * 3. Actualiza el estado
- */
 
 function App() {
   const [products, setProducts] = useState([
     {
-      id: 1,
+      id: Math.random().toString(), // Asi Usamos key unicos
       name: "iPhone",
     },
   ]);
-  const handleClick = () => {
-    // ---- Separando Componentes (Y sus Estados)----
 
-    // Donde vive el estado es donde debemos definir las acciones que lo modifican
+  const comienzo = () => {
+    const newProduct = {
+      id: Math.random().toString(),
+      name: "Inicio",
+    };
+    setProducts([newProduct, ...products]);
+  };
 
-    const newProduct = [...products, { id: 2, name: "Android" }];
-    setProducts(newProduct);
+  const final = () => {
+    const newProduct = {
+      id: Math.random().toString(),
+      name: "Final",
+    };
+    setProducts([...products, newProduct]);
+  };
+
+  const eliminar = () => {
+    const newProducts = products.slice(0, -1);
+    setProducts(newProducts);
+  };
+
+  const limpiar = () => {
+    setProducts([]);
   };
 
   return (
     <div>
-      <ProductDashboard amount={products.length} />
-      <Button onClick={handleClick}>Enviar</Button>
+      <Button onClick={comienzo}>Comienzo</Button>
+      <Button onClick={final}>Final</Button>
+      <Button onClick={eliminar}>Eliminar Ultimo</Button>
+      <Button onClick={limpiar}>Limpiar</Button>
       <ProductList products={products} />
     </div>
   );
