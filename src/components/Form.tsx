7774a019622fc9ba817d3296bcd4cca112276611
EstebanKiito <esteban.ortega@uc.cl>
2025-07-11 00:30:13 -0400

    React Hook Form: Como usar validaciones y campos controlados

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,44 +1,60 @@
-import React, { FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
 
-type Props = {};
+type Form = {
+  name: string;
+  lastname: string;
+};
 
-function Form({}: Props) {
-  // 2. CONTROLLED COMPONENT: El valor del input es controlado por el estado de React
-  // Siempre deberemos añadir 2 propiedades a los inputs: value y onChange
-  // Esto se puede mejorar con bibliotecas!
+function Form() {
+  // ---- REACT HOOK FORM ----: npm install react-hook-form (use la 7.51.3)
 
-  const [user, setUser] = useState({ name: "", lastname: "" });
+  //const form = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Form>(); // Le damos el tipo de datos que vamos a manejar en el form
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(user);
+  //console.log(register("name"));
+  // console.log(formState.errors);
+  const onSubmit = (data: Form) => {
+    console.log(data);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    //<form onSubmit={handleSubmit((data) => console.log(data))}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Nombre
         </label>
         <input
-          value={user.name} // Esto porque es un componente controlado
-          onChange={(e) => setUser({ ...user, name: e.target.value })}
+          {...register("name", {
+            minLength: {
+              value: 3,
+              message: "El nombre debe tener al menos 3 caracteres",
+            },
+          })}
           type="text"
           id="name"
           className="form-control"
         />
+        {errors?.name && <p>{errors?.name?.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="lastname" className="form-label">
           Apellido
         </label>
         <input
-          value={user.lastname} // Esto porque es un componente controlado
-          onChange={(e) => setUser({ ...user, lastname: e.target.value })}
+          {...register("lastname", {
+            validate: (valor) =>
+              valor.length < 4 ? "Largo minimo 3 caracteres" : true,
+          })} // Con esto registramos los campos del form, falta obtener los valores
           type="text"
           id="lastname"
           className="form-control"
         />
+        {errors?.lastname && <p>{errors?.lastname?.message}</p>}
       </div>
       <button className="btn btn-primary">Enviar</button>
     </form>
