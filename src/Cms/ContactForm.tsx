42379da5e94fa35b3d52f7a8baf21aa43b65816c
EstebanKiito <esteban.ortega@uc.cl>
2025-07-14 23:23:01 -0400

    Empezando Form y implementacion basica de React Hook Form en el campo de nombre

--- src/Cms/ContactForm.tsx
+++ src/Cms/ContactForm.tsx
@@ -1,7 +1,25 @@
+import { useForm } from "react-hook-form";
+
 type Props = {};
 
 function ContactForm({}: Props) {
-  return <div>ContactForm</div>;
+  // Añadimos React Hook Form
+  const methods = useForm();
+  return (
+    <form onSubmit={methods.handleSubmit((data) => console.log(data))}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Nombre
+        </label>
+        <input
+          {...methods.register("name")} // Registramos el campo "name"
+          type="name"
+          className="form-control"
+          id="name"
+        />
+      </div>
+    </form>
+  );
 }
 
 export default ContactForm;
