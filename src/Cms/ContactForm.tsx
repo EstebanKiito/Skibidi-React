6e506e1cdec9fc2f6a71257204b154b2f5c9764e
EstebanKiito <esteban.ortega@uc.cl>
2025-07-14 23:35:35 -0400

    Encapsulando Input (reutilizable): Uso de useFormContext & FormProvider

--- src/Cms/ContactForm.tsx
+++ src/Cms/ContactForm.tsx
@@ -1,24 +1,21 @@
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
+import Input from "./Input";
 
 type Props = {};
 
 function ContactForm({}: Props) {
   // Añadimos React Hook Form
   const methods = useForm();
+
+  //Agregar provider en el componente padre:
+  // 1. Shift+ctrl+p : Envolver todo con FormProvider
+  // 2. Hacer destructuring de methods
   return (
-    <form onSubmit={methods.handleSubmit((data) => console.log(data))}>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Nombre
-        </label>
-        <input
-          {...methods.register("name")} // Registramos el campo "name"
-          type="name"
-          className="form-control"
-          id="name"
-        />
-      </div>
-    </form>
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit((data) => console.log(data))}>
+        <Input name="name">Nombre</Input>
+      </form>
+    </FormProvider>
   );
 }
 
---
+++ src/Cms/Input.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+import { useFormContext } from "react-hook-form";
+
+type Props = {
+  name: string;
+  children?: ReactNode;
+};
+
+function Input({ name, children }: Props) {
+  const { register } = useFormContext(); // Obtenemos el contexto del formulario
+  // Ahora agregar provider en el componente padre
+
+  return (
+    <div className="mb-3">
+      <label htmlFor={name} className="form-label">
+        {children}
+      </label>
+      <input
+        //{...methods.register("name")} // Registramos el campo "name" -> Aqui no tenemos methods!
+        {...register("name")}
+        type="text"
+        className="form-control"
+        id={name}
+      />
+    </div>
+  );
+}
+
+export default Input;
