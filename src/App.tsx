e85fd5b78f29356b1125128813726b43661e8436
EstebanKiito <esteban.ortega@uc.cl>
2025-07-16 22:22:34 -0400

    Orden de renderizado - Efecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 function App() {
+  // 1. Primero se renderiza el componente
+  // 2. Luego de manera asincrona se ejecuta el useEffect
+
+  useEffect(() => {
+    console.log("dentro de useEffect"); // Ultimo
+  });
+  console.log("fuera de useEffect"); // Primero
+
   return <div>Hello, World!</div>;
 }
 
