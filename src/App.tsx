ab1ad643cdf2442152c489c3cda4c0f62ddd4b76
EstebanKiito <esteban.ortega@uc.cl>
2025-07-17 14:40:27 -0400

    mensaje cargando...

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ type User = {
 function App() {
   // Promesas + Consumo API
   const [users, setUsers] = useState<User[]>([]);
+  const [cargando, setCargando] = useState<boolean>(false);
 
   useEffect(() => {
     const url = "https://jsonplaceholder.typicode.com/users";
+
+    setCargando(true);
 
     // fetch + then (sin async await)
     fetch(url)
@@ -19,8 +22,16 @@ function App() {
       .then((data) => {
         setUsers(data);
         console.log(data);
+      })
+      .finally(() => {
+        setCargando(false);
       });
   }, []);
+
+  // Para un buscador lento, antes de mostrar los datos, se puede mostrar un mensaje de "CARGANDO..."
+  if (cargando) {
+    return <p>Cargando......</p>;
+  }
 
   return (
     <ul>
