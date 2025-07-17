4381baac1ab8c1b580adc0712800aa2161cec1d4
EstebanKiito <esteban.ortega@uc.cl>
2025-07-17 14:47:12 -0400

    Manejo Errores

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,34 @@ function App() {
   // Promesas + Consumo API
   const [users, setUsers] = useState<User[]>([]);
   const [cargando, setCargando] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
-    const url = "https://jsonplaceholder.typicode.com/users";
+    const url = "https://jsonplaceholder.typicode.com/xusers";
 
     setCargando(true);
 
     // fetch + then (sin async await)
     fetch(url)
       .then((response) => {
+        if (!response.ok) throw new Error(`${response.status}`);
         return response.json() as Promise<User[]>;
       })
       .then((data) => {
         setUsers(data);
         console.log(data);
       })
+      .catch((error: Error) => {
+        setError(error.message);
+      })
       .finally(() => {
         setCargando(false);
       });
   }, []);
+
+  if (error && !cargando) {
+    return <p>Error: {error}</p>;
+  }
 
   // Para un buscador lento, antes de mostrar los datos, se puede mostrar un mensaje de "CARGANDO..."
   if (cargando) {
