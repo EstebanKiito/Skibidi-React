4fc962690259988c127c38d849e2b53414181b7f
EstebanKiito <esteban.ortega@uc.cl>
2025-07-17 15:00:22 -0400

    Creando un Custom Hook: reutilizar logica

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import { useEffect, useState } from "react";
+import useUser from "./hooks/useUser";
 
-type User = {
-  id: string;
-  name: string;
-};
 function App() {
-  // Promesas + Consumo API
-  const [users, setUsers] = useState<User[]>([]);
-  const [cargando, setCargando] = useState<boolean>(false);
-  const [error, setError] = useState<string>();
-
-  useEffect(() => {
-    const url = "https://jsonplaceholder.typicode.com/xusers";
-
-    setCargando(true);
-
-    // fetch + then (sin async await)
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) throw new Error(`${response.status}`);
-        return response.json() as Promise<User[]>;
-      })
-      .then((data) => {
-        setUsers(data);
-        console.log(data);
-      })
-      .catch((error: Error) => {
-        setError(error.message);
-      })
-      .finally(() => {
-        setCargando(false);
-      });
-  }, []);
+  const { users, cargando, error } = useUser();
 
   if (error && !cargando) {
     return <p>Error: {error}</p>;
