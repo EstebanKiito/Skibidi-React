60be59b391cd2a4c4529970203566dd324a279c3
EstebanKiito <esteban.ortega@uc.cl>
2025-07-16 22:42:03 -0400

    Dependencias - Bucle de Renderizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   // 1. Primero se renderiza el componente
   // 2. Luego de manera asincrona se ejecuta el useEffect
 
   // 3. Limpieza: con una funcion anonima
-  useEffect(() => {
-    console.log("cargando usuarios..."); // Ultimo
 
-    return () => {
-      console.log("limpieza: Cancelar carga");
-    };
-  });
+  // 4. Dependencias: Debo añadir [] para que se ejecute una sola vez, si no, se queda en un loop de renderizacion infinito
+  const [users, setUsers] = useState<string[]>();
+
+  useEffect(() => {
+    console.log("Llamando al servidor...");
+    const data = ["Esteban", "Daniel"];
+    setUsers(data);
+    //}); -> Sin el argumento de dependencias, se ejecuta en cada renderizado
+  }, []);
 
   return <div>Hello, World!</div>;
 }
